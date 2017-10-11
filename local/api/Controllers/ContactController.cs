47e91722913c255c88499deacf8dfb543935dd2b
Richard Nagle <richard.nagle@firsthelpline.com>
2017-10-11 15:14:06 +0100

    create local web api to prove tests

---
+++ local/api/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using cloud.native.contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cloud.native.local.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContactController : Controller
+    {
+        //// GET api/values
+        //[HttpGet]
+        //public IEnumerable<ContactDto> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public ContactDto Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/values
+        [HttpPost]
+        public StatusCodeResult Post([FromBody]ContactDto value) => StatusCode(201);
+    }
+}
